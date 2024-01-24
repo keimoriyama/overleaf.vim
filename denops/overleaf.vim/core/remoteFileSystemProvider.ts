8d6a3fbd929aec358e87f8f919ae0e8a31c91fb9
keimoriyama <keischwiiz@gmail.com>
2024-01-25 08:59:51 +0900

    fi

--- denops/overleaf.vim/core/remoteFileSystemProvider.ts
+++ denops/overleaf.vim/core/remoteFileSystemProvider.ts
@@ -1,74 +1,74 @@
-// // remoteのファイルをローカルで処理するためのファイルシステムの構築をしている
-// import {
-//   ExtendedBaseAPI,
-//   ProjectLinkedFileProvider,
-//   UrlLinkedFileProvider,
-// } from "../api/extendedBase.ts";
-// import { GlobalStateManager } from '../utils/globalStateManager.ts';
-// import { MemberEntity, ProjectSettingsSchema } from "../api/base.ts";
-//
-// export type FileType = "doc" | "file" | "folder" | "outputs";
-// export type FolderKey = "docs" | "fileRefs" | "folders" | "outputs";
-// const FolderKeys: {[_type:string]: FolderKey} = {
-// 	'folder': "folders",
-// 	"doc": "docs",
-// 	"file": "fileRefs",
-// 	"outputs": "outputs"
-// }
-//
-// export interface FileEntity {
-//   _id: string;
-//   name: string;
-//   _type?: FileType;
-//   readonly?: boolean;
-// }
-//
-// export interface DocumentEntity extends FileEntity {
-//   version?: number;
-//   mtime?: number;
-//   lastVersion?: number;
-//   localCache?: string;
-//   remoteCache?: string;
-// }
-//
-// export interface FileRefEntity extends FileEntity {
-//   linkedFileData: ProjectLinkedFileProvider | UrlLinkedFileProvider | null;
-//   created: string;
-// }
-//
-// export interface OutputFileEntity extends FileEntity {
-//   path: string;
-//   url: string;
-//   type: string;
-//   build: string;
-// }
-//
-// export interface FolderEntity extends FileEntity {
-//   docs: Array<DocumentEntity>;
-//   fileRefs: Array<FileRefEntity>;
-//   folders: Array<FolderEntity>;
-//   outputs?: Array<OutputFileEntity>;
-// }
-//
-// export interface ProjectEntity {
-//   _id: string;
-//   name: string;
-//   rootDoc_id: string;
-//   rootFolder: Array<FolderEntity>;
-//   publicAccessLevel: string; //"tokenBased"
-//   compiler: string;
-//   spellCheckLanguage: string;
-//   deletedDocs: Array<{
-//     _id: string;
-//     name: string;
-//     deletedAt: string;
-//   }>;
-//   members: Array<MemberEntity>;
-//   invites: Array<MemberEntity>;
-//   owner: MemberEntity;
-//   features: { [key: string]: any };
-//   settings: ProjectSettingsSchema;
-// }
+// remoteのファイルをローカルで処理するためのファイルシステムの構築をしている
+import {
+  ExtendedBaseAPI,
+  ProjectLinkedFileProvider,
+  UrlLinkedFileProvider,
+} from "../api/extendedBase.ts";
+import { GlobalStateManager } from '../utils/globalStateManager.ts';
+import { MemberEntity, ProjectSettingsSchema } from "../api/base.ts";
+
+export type FileType = "doc" | "file" | "folder" | "outputs";
+export type FolderKey = "docs" | "fileRefs" | "folders" | "outputs";
+const FolderKeys: {[_type:string]: FolderKey} = {
+	'folder': "folders",
+	"doc": "docs",
+	"file": "fileRefs",
+	"outputs": "outputs"
+}
+
+export interface FileEntity {
+  _id: string;
+  name: string;
+  _type?: FileType;
+  readonly?: boolean;
+}
+
+export interface DocumentEntity extends FileEntity {
+  version?: number;
+  mtime?: number;
+  lastVersion?: number;
+  localCache?: string;
+  remoteCache?: string;
+}
+
+export interface FileRefEntity extends FileEntity {
+  linkedFileData: ProjectLinkedFileProvider | UrlLinkedFileProvider | null;
+  created: string;
+}
+
+export interface OutputFileEntity extends FileEntity {
+  path: string;
+  url: string;
+  type: string;
+  build: string;
+}
+
+export interface FolderEntity extends FileEntity {
+  docs: Array<DocumentEntity>;
+  fileRefs: Array<FileRefEntity>;
+  folders: Array<FolderEntity>;
+  outputs?: Array<OutputFileEntity>;
+}
+
+export interface ProjectEntity {
+  _id: string;
+  name: string;
+  rootDoc_id: string;
+  rootFolder: Array<FolderEntity>;
+  publicAccessLevel: string; //"tokenBased"
+  compiler: string;
+  spellCheckLanguage: string;
+  deletedDocs: Array<{
+    _id: string;
+    name: string;
+    deletedAt: string;
+  }>;
+  members: Array<MemberEntity>;
+  invites: Array<MemberEntity>;
+  owner: MemberEntity;
+  features: { [key: string]: any };
+  settings: ProjectSettingsSchema;
+}
 //
 // export class File{
 // 	name:string;
